efcb3b54a01e9a04cee742c493ee61d0d94c89df
zhangshaobin <zhangshaobin@blephone.com>
2018-08-27 10:44:24 +0800

    修改查询信息

--- Server/ota/routes/ota_test.js
+++ Server/ota/routes/ota_test.js
@@ -14,6 +14,9 @@ var OTA_HOST = 'localhost';
 var OTA_DATABASE = 'ota';
 var OTA_USERNAME = 'root';
 var OTA_PWD = 'youqin';
+var OTA_TABLE = 'updateInfo_test';
+
+var dlUrlBase = 'http://121.43.183.196:8081/updsvr/ota/test/ota_file?file=';
 
 /* GET OTA UPDATE */
 router.get('/checkupdate', function(req, res, next) {
@@ -24,11 +27,12 @@ router.get('/checkupdate', function(req, res, next) {
     var serial = req.query.serialno;
     var sessionId = req.sessionID;
     var model = req.query.model;
-    var sql = 'SELECT * FROM updateInfo WHERE (hardWare = "' + hardWare + '" AND model = "' + model + '")';
+    var sql = 'SELECT * FROM ' + OTA_TABLE + ' WHERE (hardWare = "' + hardWare + '" AND model = "' + model + '" AND srcVersion = "' + softWareVersion + '")';
     
     var info = {
         'OTA_NAME':'blephone_ota',
         'CHECK_UPDATE_RESULT':'-1',
+        'hardware':'',
         'model':'',
         'fileName':'',
         'filePath':'',
@@ -47,6 +51,8 @@ router.get('/checkupdate', function(req, res, next) {
     req.session.user = serial;
     logger.info('req.session.user : ' + req.session.user);
     info.sessionId = sessionId;
+    info.model = model;
+    info.hardware = hardWare;
     
     var connection = mysql.createConnection({
         host     : OTA_HOST,
@@ -57,88 +63,98 @@ router.get('/checkupdate', function(req, res, next) {
     
     connection.connect();
     
-    connection.query(sql,function (err, result) {
+    connection.query(sql, function (err, result) {
         if(err){
             logger.error('get update info failed! err.message:' + err.message);
+            logger.info('connection.query error');
             info.CHECK_UPDATE_RESULT = '-1';
-            info.description = 'get ota info failed!';
-            var bd = new xml2js.Builder();
-            var xml = bd.buildObject(info);
-            
-            res.writeHead(200, {'Content-Type': 'application/json;charset=utf-8'});
-            res.end(JSON.stringify(info));
-            return;
+            info.description = 'check ota info from database failed!';
+            returnXmlHead(res, info);
         }else{
-            info.srcVersion = result[0].srcVersion;
-            info.dstVersion = result[0].dstVersion;
-            info.model = result[0].model;
-            
-            if(softWareVersion == info.srcVersion){
-                info.CHECK_UPDATE_RESULT = '0';
-                info.fileName = result[0].fileName;
-                info.filePath = result[0].filePath;
-                info.description = result[0].description;
-                info.priority = result[0].priority;
-                
-                //var dir = process.cwd() + '/ota_file';
-                var filePath = process.cwd() + result[0].filePath;
-                var md5 = getFileMD5(filePath);
-                logger.info('filePath : ' + filePath);
-                var otafile = fs.statSync(filePath);
-                info.size = otafile.size + "byte";
-                logger.info('file size : ' + info.size);
-                info.md5 = md5;
-                logger.info('md5 : ' + md5);
-                info.downloadUrl = 'http://121.43.183.196:8081/updsvr/ota/test/ota_test_file?' + 'file=' + info.fileName;
-                logger.info('downloadUrl : ' + info.downloadUrl);
-            }else if(softWareVersion == info.dstVersion){
-                logger.error('software is already updated! ver:' + softWareVersion);
-                info.CHECK_UPDATE_RESULT = '1';
-                info.description = 'software is already updated!';
-            }else{
-                logger.error('wrong target software version! ver:' + softWareVersion);
+            var retLength = result.length;
+            if(retLength <= 0){
+                logger.info('connection.query result is null');
                 info.CHECK_UPDATE_RESULT = '-1';
-                info.description = 'wrong target software version!';
+                info.description = 'check ota info from database failed!';
+                returnXmlHead(res, info);
+            }else{
+                info.srcVersion = result[0].srcVersion;
+                info.dstVersion = result[0].dstVersion;
+                
+                if(softWareVersion == info.srcVersion){
+                    var mfilePath = process.cwd() + result[0].filePath;
+                    logger.info('load mysql filePath:' + mfilePath);
+                    fs.exists(mfilePath, function(exist) {
+                        if (exist) {
+                            logger.info('exist ota pkg:' + mfilePath);
+                            info.CHECK_UPDATE_RESULT = '0';
+                            info.fileName = result[0].fileName;
+                            info.filePath = result[0].filePath;
+                            info.description = result[0].description;
+                            info.priority = result[0].priority;
+                            //var md5 = getFileMD5(mfilePath);
+                            var otafile = fs.statSync(mfilePath);
+                            info.size = otafile.size + "byte";
+                            //info.size = result[0].size;
+                            logger.info('file size : ' + info.size);
+                            info.md5 = result[0].md5;
+                            logger.info('md5 : ' + info.md5);
+                            //info.downloadUrl = dlUrlBase + info.filePath;
+                            info.downloadUrl = result[0].downloadUrl;
+                            logger.info('downloadUrl : ' + info.downloadUrl);
+                        } else {
+                            logger.info('OTA pkg File not exist!');
+                            info.CHECK_UPDATE_RESULT = '-1';
+                            info.description = 'ota pkg file is not exist!';
+                        }
+                        returnXmlHead(res, info);
+                    });
+                }else if(softWareVersion == info.dstVersion){
+                    logger.info('software is already updated! ver:' + softWareVersion);
+                    info.CHECK_UPDATE_RESULT = '1';
+                    info.description = 'software is already updated!';
+                    returnXmlHead(res, info);
+                }else{
+                    logger.info('wrong target software version! ver:' + softWareVersion);
+                    info.CHECK_UPDATE_RESULT = '-1';
+                    info.description = 'wrong target software version!';
+                    returnXmlHead(res, info);
+                }
             }
-            
-            var bd = new xml2js.Builder();
-            var xml = bd.buildObject(info);
-            
-            res.writeHead(200, {'Content-Type': 'application/json;charset=utf-8'});
-            res.end(JSON.stringify(info));
         }
     });
     
     connection.end();
 });
 
+/* switch json to xml */
+function returnXmlHead(response, infos) {
+    var bd = new xml2js.Builder();
+    var xml = bd.buildObject(infos);
+    logger.info('----function returnXmlHead----');
+    response.writeHead(200, {'Content-Type': 'application/json;charset=utf-8'});
+    response.end(JSON.stringify(infos));
+}
+
 /* GET OTA FILE */
-router.get('/ota_test_file', function(req, res, next) {
+router.get('/ota_file', function(req, res, next) {
     var ssid = req.query.ssid;
-    var filename = req.query.file;
+    var filepath = req.query.file;
     logger.info('download ssid : ' + ssid);
     logger.info('login ssid : ' + req.sessionID);
-    logger.info('filename : ' + filename);
-    var dir = process.cwd() + '/ota_test_file'
-    var filePath = path.join(dir, filename);
+    logger.info('filepath : ' + filepath);
+    var dlpath = process.cwd() + filepath;
     
-    fs.exists(filePath, function(exist) {
+    fs.exists(dlpath, function(exist) {
         if (exist) {
-            logger.info('downloading:' + filename);
-            res.download(filePath); 
-        }
-        else {
+            logger.info('downloading:' + filepath);
+            res.download(dlpath); 
+        } else {
             logger.error('File not exist!');
             res.set('Content-type', 'text/html');
             res.end('File not exist.');
         }
     });
-    /*if(ssid == req.sessionID){
-        
-    }else{
-        res.send('ssid is wrong! ' + ssid + ':' + req.sessionID);
-        res.end();
-    }*/
 });
 
 /* GET SESSION */
